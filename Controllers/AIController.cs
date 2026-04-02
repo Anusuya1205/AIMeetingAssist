5e53af074456c90b21187a0340aba597cc2621cf
Anusuya <anuofficial2000@gmail.com>
2026-04-02 22:02:44 +0530

    Api Setup done

---
+++ Controllers/AIController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIMeetingAssist.Controllers
+{
+   [Route("api/[controller]")]
+
+    [ApiController]
+    public class AIController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Api is Working");
+        }
+    }
+}
